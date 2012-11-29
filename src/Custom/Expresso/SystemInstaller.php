bdb52481707e8141cc28e955f2b437994e611108
Adir Kuhn <adirkuhn@tardis.(none)>
2012-11-29 15:09:59 -0200

    teste

--- src/Custom/Expresso/SystemInstaller.php
+++ src/Custom/Expresso/SystemInstaller.php
@@ -11,15 +11,6 @@ class SystemInstaller extends LibraryInstaller
      */
     public function getInstallPath(PackageInterface $package)
     {
-        $prefix = substr($package->getPrettyName(), 0, 23);
-        if ('phpdocumentor/template-' !== $prefix) {
-            throw new \InvalidArgumentException(
-                'Unable to install template, phpdocumentor templates '
-                .'should always start their package name with '
-                .'"phpdocumentor/template-"'
-            );
-        }
-
         return 'data/templates/'.substr($package->getPrettyName(), 23);
     }
 
